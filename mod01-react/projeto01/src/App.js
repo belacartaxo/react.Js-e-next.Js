6cc5a2f2e05432e20edc2af7c45bf8c27c75fced
Isabela Britto Cartaxo <111789361+belacartaxo@users.noreply.github.com>
2023-03-20 22:16:55 -0300

    att

--- mod01-react/projeto01/src/App.js
+++ mod01-react/projeto01/src/App.js
@@ -1,49 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
+  state = {
+      posts: [
+        {
+          id:1,
+          title:'O título 1',
+          body: 'O corpo 1'
+        },
+        {
+          id:2,
+          title:'O título 2',
+          body: 'O corpo 2'
+        },
+        {
+          id:3,
+          title:'O título 3',
+          body: 'O corpo 3'
+        }
+      ]
+  };
+
+  //Para fazer com que algo ocorra assim que o componente for montado na tela
+  componentDidMount() {
+    console.log('oi');
+  }
+
   render(){
+  const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
 }
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
 
 export default App;
