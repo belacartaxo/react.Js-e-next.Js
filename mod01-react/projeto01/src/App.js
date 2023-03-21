22a35c867ecbc0b2a77e266758c239cd139607a1
Isabela Britto Cartaxo <111789361+belacartaxo@users.noreply.github.com>
2023-03-21 19:27:38 -0300

    aula 21

--- mod01-react/projeto01/src/App.js
+++ mod01-react/projeto01/src/App.js
@@ -1,29 +1,19 @@
 import './App.css';
 import { Component } from 'react';
-import { PostCard } from './components/PostCard';
+import {loadPosts} from './utils/load-post'
+import { Posts } from './components/Posts';
 
 class App extends Component{
   state = {
       posts: []
   };
 
-  componentDidMount() {
-    this.loadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
   
-  loadPosts = async ()=>{ //requisições posts
-    const postResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) =>{ //para cada post pega um índice que corresponde a uma foto
-      return { ...post, cover: photosJson[index].url}
-    });
-
+  loadPosts = async ()=>{
+    const postsAndPhotos = await loadPosts();
     this.setState({posts: postsAndPhotos});
   }
 
@@ -31,17 +21,7 @@ class App extends Component{
   const { posts } = this.state;
     return (
       <section className='container'>
-        <div className="posts">
-          {posts.map(post => (
-            <PostCard
-              //passando os dados par o componente, esses dados ficam armazenado no objeto props
-              key={post.id}
-              title={post.title}
-              body={post.body}
-              cover={post.cover}
-            />
-          ))}
-        </div>
+        <Posts posts={posts}/>
       </section>
     );
   }
