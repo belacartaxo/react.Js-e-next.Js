c717b6950dee5b833f42129b8d67bd00bad270c9
Isabela Britto Cartaxo <111789361+belacartaxo@users.noreply.github.com>
2023-03-22 17:52:46 -0300

    att

--- mod01-react/projeto01/src/templates/Home/index.jsx
+++ mod01-react/projeto01/src/templates/Home/index.jsx
@@ -3,13 +3,15 @@ import './styles.css';
 import { Posts } from '../../components/Posts';
 import {loadPosts} from '../../utils/load-post'
 import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
 export class Home extends Component{
   state = {
       posts: [],
       allPosts:[],
       page: 0,
-      postsPerPage: 10
+      postsPerPage: 10,
+      searchValue: ''
   };
 
   async componentDidMount() {
@@ -39,19 +41,50 @@ export class Home extends Component{
     this.setState({posts, page:nextPage})
   }
 
+  handleChange = (e) =>{
+    const {value} = e.target;
+    this.setState({searchValue: value})
+  }
+
   render(){
-  const { posts, page, postsPerPage, allPosts } = this.state;
-  const noMorePosts = page + postsPerPage >= allPosts.length
+  const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? allPosts.filter(post =>{
+    return post.title.toLowerCase().includes(searchValue.toLowerCase());
+  }) : posts;
+  //se tiver algum valor no search value vai acontecer a coisa x, e se não, a y
 
     return (
       <section className='container'>
-        <Posts posts={posts}/>
-        <div className="button-container">
-          <Button 
-            text="Load More Posts" 
-            onClick={this.loadMorePosts} /*esse onClick não é um evento, é um atributo que está sendo mandado para a props do componente*/
-            disabled={noMorePosts}
+        <div className='search-container'>
+          {!!searchValue && (//se o search value for verdadeiro faço o seguinte:
+          //!!- transforma o valor do searchvalue em um valor booleano
+            <h1>Busca:</h1>
+          )}
+
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
           />
+        </div>
+
+        {filteredPosts.length > 0 &&( // se filtered posts for maior que zero faça isso:
+          <Posts posts={filteredPosts}/>
+        )}
+
+        {filteredPosts.length === 0 &&( // se filtered posts for IGUAL A zero faça isso:
+          <p className='filtered-posts'>Não existem posts</p>
+        )}
+
+        <div className="button-container">
+          {!searchValue && (//se não tiver o searchvalue o botão será inserido
+            <Button 
+              text="Load More Posts" 
+              onClick={this.loadMorePosts} /*esse onClick não é um evento, é um atributo que está sendo mandado para a props do componente*/
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
